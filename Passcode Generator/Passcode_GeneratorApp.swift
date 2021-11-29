2354e094628e9889dd5d549614e3483fb8e2e412
Vaida <Fan_Vaida@iCloud.com>
2021-11-30 02:27:15 +0800

    Initial Commit

---
+++ Passcode Generator/Passcode_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Passcode_GeneratorApp.swift
+//  Passcode Generator
+//
+//  Created by Vaida on 11/30/21.
+//
+
+import SwiftUI
+
+@main
+struct Passcode_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
